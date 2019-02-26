d4c7f5bb40f71f3388e777744250910e4815b206
Serge Markov <serge.markov+git@gmail.com>
2019-02-26 18:22:18 +0700

    HW3 - easyjson + write directly

--- hw3_bench/fast.go
+++ hw3_bench/fast.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
-	"regexp"
 	"strings"
 )
+
+//easyjson:json
+type User struct {
+	Browsers []string `json:"browsers"`
+	//Company  string   `json:"company"`
+	//Country  string   `json:"country"`
+	Email string `json:"email"`
+	//Job      string   `json:"job"`
+	Name string `json:"name"`
+	//Phone    string   `json:"phone"`
+}
 
 // вам надо написать более быструю оптимальную этой функции
 func FastSearch(out io.Writer) {
@@ -17,40 +26,23 @@ func FastSearch(out io.Writer) {
 		panic(err)
 	}
 
-	fileContents, err := ioutil.ReadAll(file)
-	if err != nil {
-		panic(err)
-	}
-
 	matchBrowsers := []string{"Android", "MSIE"}
-	seenBrowsers := make(map[string]bool, 1000) // preallocate for 1000 browsers
-	foundUsers := ""
-	r := regexp.MustCompile("@")
-	// TODO Buffer
-	lines := strings.Split(string(fileContents), "\n")
-	i := -1 // headers
+	seenBrowsers := make(map[string]bool, 128) // preallocate for 1000 browsers
+	i := -1                                    // headers
+	scanner := bufio.NewScanner(file)
+	user := User{}
+	fmt.Fprint(out, "found users:\n")
+
 USERS:
-	for _, line := range lines {
+	for scanner.Scan() {
 		i++
-		user := make(map[string]interface{})
-		// fmt.Printf("%v %v\n", err, line)
-		err := json.Unmarshal([]byte(line), &user)
+		err := user.UnmarshalJSON(scanner.Bytes())
 		if err != nil {
 			panic(err)
 		}
 		matched := []bool{false, false}
-		browsers, ok := user["browsers"].([]interface{})
-		if !ok {
-			// log.Println("cant cast browsers")
-			continue
-		}
 		//BROWSERS:
-		for _, browserRaw := range browsers {
-			browser, ok := browserRaw.(string)
-			if !ok {
-				// log.Println("cant cast browser to string")
-				continue
-			}
+		for _, browser := range user.Browsers {
 			for i, match := range matchBrowsers {
 				if strings.Contains(browser, match) {
 					matched[i] = true
@@ -59,14 +51,20 @@ USERS:
 			}
 			if matched[0] && matched[1] { // at some point we found - no need to go further - can be many AND
 				// log.Println("Android and MSIE user:", user["name"], user["email"])
-				email := r.ReplaceAllString(user["email"].(string), " [at] ")
-				foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i, user["name"], email)
+				// TODO - buffer?
+				fmt.Fprintf(out, "[%d] %s <%s>\n", i, user.Name, strings.Replace(user.Email, "@", " [at] ", 1))
 				continue USERS
 			}
 		}
 
 	}
-
-	fmt.Fprintln(out, "found users:\n"+foundUsers)
-	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+	fmt.Fprintln(out, "\nTotal unique browsers", len(seenBrowsers))
 }
+
+/*
+BenchmarkSlow-8               10         105465760 ns/op        319474528 B/op    276173 allocs/op
+BenchmarkFast-8              100          11389521 ns/op         5437302 B/op      59758 allocs/op
+*/
